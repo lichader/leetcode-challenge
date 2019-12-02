5ac4dbd69874b6e03fcc6940f606ce2b703d7634
Lichader <1524470+lichader@users.noreply.github.com>
2019-12-02 21:36:02 +1100

    First version

--- problems/two_sum.py
+++ problems/two_sum.py
@@ -12,5 +12,14 @@ class Solution:
         return [0, 1].
     """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-        return []
+        length = len(nums)
+
+        for i in range(length):
+            num1 = nums[i]
+            for j in range(i+1, length):
+                num2 = nums[j]
+
+                if (num1 + num2) == target:
+                    return [i, j]
+
+        return [0, 0]
--- tests/01_two_num_test.py
+++ tests/01_two_num_test.py
@@ -1,7 +1,13 @@
 from problems.two_sum import Solution
 
-def test_no1():
+def test_sample_1():
     sol = Solution()
 
-    result = sol.twoSum([], 1)
-    assert len(result) != 0
+    result = sol.twoSum([2, 7, 11, 15], 9)
+    assert result == [0, 1]
+
+def test_sample_2():
+    sol = Solution()
+
+    result = sol.twoSum([1, 4, 0, 9], 9)
+    assert result == [2, 3]
